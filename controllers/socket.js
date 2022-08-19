59cfdcbf867b20c545447d6db76a94c2d4b32446
Leonardo <qqjq9904@gmail.com>
2022-08-18 21:39:00 -0500

    ok peticion de lado a lado

--- controllers/socket.js
+++ controllers/socket.js
@@ -2,21 +2,21 @@ const socketController = (socket)=>{
   console.log('cliente conectado');
   console.log(socket.id)
   socket.on('disconnect',()=>{
-      console.log('Cliente desconetado', socket)
+      console.log('Cliente desconetado')
   })
 
   socket.on('registro',(payload,callback)=>{
     socket.join(payload.id)
     callback(payload.id)
     socket.emit('registro',payload)
+    console.log('se llego a registro')
+    console.log(socket.rooms)
   })
 
   socket.on('enviar-mensaje', ( payload, callback ) => {
-                
       callback( socket.id );
-
       socket.to(payload.to).emit('enviar-mensaje', payload );
-
+      console.log('entro peticion de enviar mensaje',payload)
   })
 }
 
